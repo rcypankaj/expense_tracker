a0a6dd793b1b83c53c1d970d996216f840119413
Pankaj <rcypankaj@gmail.com>
2023-02-25 23:01:05 +0530

    Initial Commit

---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,24 @@
+import "./ExpensesFilter.css";
+
+const ExpensesFilter = (props) => {
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+  console.log(props);
+
+  return (
+    <div className="expense-filter">
+      <div className="expense-filter__control">
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
